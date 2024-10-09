f465d50a71ae1f10e39ebf9349004394f25cb1ed
Drew Igoe <drewigoe@gmail.com>
2024-10-08 19:59:39 -0700

    Initial Commit

---
+++ Image Classifier/Image_ClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_ClassifierApp.swift
+//  Image Classifier
+//
+//  Created by Drew Igoe on 10/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Image_ClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
